60bdd69997b2c0302a1158030a922f96f4df7796
Yontwises <kenny.yontwises@gmail.com>
2020-06-07 16:07:54 -0700

    deleted insertcolorhere.hpp files bc why do we need them

--- chips.hpp
+++ chips.hpp
@@ -20,5 +20,149 @@ class Chips {
         virtual void subtract(int num) = 0;
 };
 
+class white : public Chips {  
+    private:
+        int dollarAmount; //total amount of chips in dollar units
+        int whiteCount; 
+    public:
+        white() {
+            dollarAmount = 0;
+            whiteCount = 0;
+        }
+         virtual void add(int num) {
+            whiteCount = whiteCount + num;
+            setDollarAmount();
+        }
+        virtual void subtract(int num) {
+            whiteCount = whiteCount - num;
+            setDollarAmount();
+        }
 
+         virtual void accept(Visitor* v,int num) {
+            v->visit(this, num);
+        }
+
+          void setDollarAmount() {
+            dollarAmount = whiteCount * 1;
+        }
+
+
+
+};
+
+class red : public Chips {
+    private:
+        int dollarAmount; //total amount of chips in dollar units
+        int redCount; 
+    public:
+        red() {
+            dollarAmount = 0;
+            redCount = 0;
+        }
+        virtual void add(int num) {
+            redCount = redCount + num;
+            setDollarAmount();
+        }
+        virtual void subtract(int num) {
+            redCount = redCount - num;
+            setDollarAmount();
+        }
+
+         virtual void accept(Visitor* v,int num) {
+            v->visit(this, num);
+        }
+          void setDollarAmount() {
+            dollarAmount = redCount * 5;
+        }
+        
+};
+
+class blue : public Chips {
+    private: 
+        int dollarAmount; //total amount of chips in dollar units
+        int blueCount; 
+    public:
+        blue() {
+            dollarAmount = 0;
+            blueCount = 0;
+
+        }
+        virtual void add(int num) {
+            blueCount = blueCount + num;
+            setDollarAmount();
+        }
+        virtual void subtract(int num) {
+            blueCount = blueCount - num;
+            setDollarAmount();
+        }
+         virtual void accept(Visitor* v,int num) {
+            v->visit(this, num);
+        }
+
+        void setDollarAmount() {
+            dollarAmount = blueCount * 10;
+        }
+
+
+
+};
+
+class green : public Chips {
+    private: 
+        int dollarAmount; //total amount of chips in dollar units
+        int greenCount; 
+    public: 
+        green() {
+            dollarAmount = 0;
+            greenCount = 0;
+        }
+        virtual void add(int num) {
+            greenCount = greenCount + num;
+            setDollarAmount();
+        }
+        virtual void subtract(int num) {
+            greenCount = greenCount - num;
+            setDollarAmount();
+        }
+
+         virtual void accept(Visitor* v,int num) {
+            v->visit(this, num);
+        }
+
+          void setDollarAmount() {
+            dollarAmount = greenCount * 25;
+        }
+
+
+};
+
+class black : public Chips {
+    private:
+        int dollarAmount; //dollar amount 
+        int blackCount; 
+    public: 
+        black() {
+            blackCount = 0;
+            dollarAmount = 0;
+        }
+        virtual void add(int num) {
+            blackCount = blackCount - num;
+            setDollarAmount();
+        }
+        virtual void subtract(int num) {
+            blackCount = blackCount - num;
+             setDollarAmount();
+        }
+
+        virtual void accept(Visitor* v,int num) {
+            v->visit(this, num);
+        }
+
+        void setDollarAmount() {
+            dollarAmount = blackCount * 100;
+        }
+
+
+    
+};
 #endif
