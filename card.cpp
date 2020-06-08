968b2f4b75f4af2bc9aea57ee8d9a326fda3e1f3
KennyXLay <KxLay99@gmail.com>
2020-06-08 00:33:35 -0700

    Add deck and card

---
+++ card.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<cctype>
+using namespace std;
+
+#include "Card.h"
+
+
+Card::Card(){
+  suit = 'c';
+  rank = 2;
+}
+
+Card::Card(char userSuit, int userNum){
+  if ((tolower(userSuit) != 'c') && (tolower(userSuit) != 'd') && (tolower(userSuit) != 'h') && (tolower(userSuit) != 's')) {
+    userSuit = 'c';
+  }
+  if ((userNum < 1) || (userNum > 13)) {
+    userNum = 2;
+  }
+  suit = tolower(userSuit);
+  rank = userNum;
+}
+
+char Card::getSuit() const{
+  return suit;
+}
+
+int Card::getRank() const{
+  return rank;
+}
+
+ostream & operator<<(ostream & fout, const Card &suitRank ){
+  string temp;
+  if(suitRank.getSuit() == 's'){
+    temp = "Spades";
+  }
+  else if(suitRank.getSuit() == 'h'){
+    temp = "Hearts";
+  }
+  else if(suitRank.getSuit() == 'd'){
+    temp = "Diamonds";
+  }
+  else if(suitRank.getSuit() == 'c'){
+    temp = "Clubs";
+  }
+  else{
+    temp = "Unknown";
+  }
+  if(suitRank.getRank() == 1){
+    fout << "Ace";
+  }
+  else if(suitRank.getRank() == 11){
+    fout << "Jack";
+  }
+  else if(suitRank.getRank() == 12){
+    fout << "Queen";
+  }
+  else if(suitRank.getRank() == 13){
+    fout << "King";
+  }
+  else{
+    fout << suitRank.getRank();
+  }
+  fout << " of " << temp;
+  return fout;
+}
+
